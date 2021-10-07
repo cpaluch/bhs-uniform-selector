d0d131aba5a10df295766aba354a5fad1462fdb6
cpaluch <cpaluch@vt.edu>
2021-10-07 13:51:31 -0400

    Add grid layout to list

--- mern/react-app/src/components/UniformItem.js
+++ mern/react-app/src/components/UniformItem.js
@@ -4,17 +4,18 @@ import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-export default function UniformItem({uniform}) {
+export default function UniformItem({ uniform }) {
     return (
         <>
-            <Card sx={{minWidth: 275,  p: 2}}>
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly'}}>
-                    <div>
+
+            <Card variant='outlined' sx={{minWidth: 275,  p: 2}}>
+                <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)'}}>
+                    <div align='center'>
                         <Checkbox size="large"></Checkbox>
                     </div>
-                    <Typography>{uniform.type}</Typography>
-                    <Typography>{uniform.id}</Typography>
-                    <Typography>{uniform.size}</Typography>
+                    <Typography sx={{m: 'auto'}}>{uniform.type}</Typography>
+                    <Typography sx={{m: 'auto'}}>{uniform.id}</Typography>
+                    <Typography sx={{m: 'auto'}}>{uniform.size}</Typography>
                 </Box>
             </Card>
         </>
--- mern/react-app/src/components/UniformList.js
+++ mern/react-app/src/components/UniformList.js
@@ -1,15 +1,48 @@
 import React from 'react'
 import UniformItem from './UniformItem'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 
-const uniform = {
-    type: 'Jacket',
-    id: '61',
-    size: 'large'
-};
 export default function UniformList({ uniforms }) {
     return (
-        uniforms.map(uniform => {
-            return <UniformItem uniform={ uniform }></UniformItem>
-        })
+
+
+        <Box sx={{
+            width: '400px',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(5, 1fr)',
+            gap: 1,
+            gridTemplateRows: 'auto',
+            gridTemplateAreas: `"title title . . filter"
+          "content content content content content"
+          ". . . assign assign"`,
+          }}>
+
+            <Box sx={{ gridArea: 'title', backgroundColor: 'blue'}}>
+                <Typography variant='h1' align='center' sx={{ color:'white', fontSize: 'x-large', p:2}}>Uniforms</Typography>
+            </Box>
+
+            <Box sx={{ gridArea: 'filter'}}>Filter</Box>
+
+            <Card sx={{ gridArea: 'content'}}>
+                <CardContent sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)'}}>
+                    <Typography variant='h2'align='center' sx={{ fontWeight: 'bold', fontSize: 'large'}}>Select</Typography>
+                    <Typography variant='h2'align='center' sx={{ fontWeight: 'bold', fontSize: 'large'}}>Type</Typography>
+                    <Typography variant='h2'align='center' sx={{ fontWeight: 'bold', fontSize: 'large'}}>Uniform ID</Typography>
+                    <Typography variant='h2'align='center' sx={{ fontWeight: 'bold', fontSize: 'large'}}>Size</Typography>
+                </CardContent>
+
+                { uniforms.map(uniform => {
+                    return <UniformItem uniform={ uniform }></UniformItem>
+                })}
+            </Card>
+
+            <Box sx={{ gridArea: 'assign'}}>
+                assignButton
+            </Box>
+        </Box>
+        
     )
 }
