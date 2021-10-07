6f20edee200900a14cb21fa4dd2de9f454819cec
Noah Hefner <noah.hefner127@gmail.com>
2021-10-07 12:58:43 -0400

    removed react logos, removed testing code from App.js/css

--- mern/react-app/src/App.js
+++ mern/react-app/src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import styles from './App.module.css';
-import { UniformAssignPage } from './UniformAssignPage';
 
 function App() {
 
-  return (
-    <div className={styles.float_container}>
-      <UniformAssignPage/>
-    </div>
-  )
+  return null
 }
 
 export default App;
