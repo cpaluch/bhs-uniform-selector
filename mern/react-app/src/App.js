172f8974e9b37d5005e263117039fd6b021a1b3e
Noah Hefner <noah.hefner127@gmail.com>
2021-10-07 16:11:59 -0400

    removed test content from App.js

--- mern/react-app/src/App.js
+++ mern/react-app/src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { UniformAssignPage } from './UniformAssignPage';
 
 function App() {
 
-  return (
-    <div>
-      <UniformAssignPage/>
-    </div>
-  );
+  return null;
 }
 
 export default App;
