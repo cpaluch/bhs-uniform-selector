d1907a2cb2e4ae4a80ec9e1ae9c7f60488d1084e
anelli55 <anelli55@vt.edu>
2021-10-13 14:29:43 -0400

    removed test code

--- mern/react-app/src/App.js
+++ mern/react-app/src/App.js
@@ -1,15 +1,1 @@
-import React, { Component } from 'react'
-import Footer from './Footer'
-import Header from './Header'
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-          <Header/>
-          <Footer/>
-      </div>
-    )
-  }
-}
 
