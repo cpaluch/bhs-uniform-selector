603cce67e5067053b858d986a6c243342ac42995
cpaluch <cpaluch@vt.edu>
2021-10-13 14:56:08 -0400

    Refactored List to use MUI tables

--- mern/react-app/src/components/UniformList.js
+++ mern/react-app/src/components/UniformList.js
@@ -1,50 +1,364 @@
 import React from 'react'
-import UniformItem from './UniformItem'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-
 import Button from '@mui/material/Button';
 
-export default function UniformList({ uniforms }) {
+import PropTypes from 'prop-types';
+import { alpha, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+
+  function createData(type, id, size) {
+    return {
+      type,
+      id,
+      size
+    };
+  }
+  
+  const rows = [
+    createData('Jacket', 2, "Large"),
+    createData('Jacket', 3, "Medium"),
+    createData('Jacket', 4, "Small"),
+    createData('Jacket', 5, "X-Large"),
+    createData('Hat', 11, "Large"),
+    createData('Hat', 12, "Medium"),
+    createData('Hat', 14, "Small"),
+    createData('Hat', 22, "X-Large"),
+    createData('Gauntlet', 24, "Large"),
+    createData('Gauntlet', 6, "Medium"),
+    createData('Gauntlet', 55, "Small"),
+    createData('Gauntlet', 23, "X-Large"),
+  ];
+  
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+  
+  // This method is created for cross-browser compatibility, if you don't
+  // need to support IE11, you can use Array.prototype.sort() directly
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+  
+  const headCells = [
+    {
+      id: 'type',
+      numeric: false,
+      disablePadding: true,
+      label: 'Type',
+    },
+    {
+      id: 'id',
+      numeric: true,
+      disablePadding: false,
+      label: 'Uniform ID',
+    },
+    {
+      id: 'size',
+      numeric: true,
+      disablePadding: false,
+      label: 'Size',
+    }
+  ];
+  
+  function EnhancedTableHead(props) {
+    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+      props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+  
     return (
-        <Box sx={{
-            width: '400px',
-            display: 'grid',
-            gridTemplateColumns: 'repeat(5, 1fr)',
-            gap: 1,
-            gridTemplateRows: 'auto',
-            gridTemplateAreas: `"title title . . filter"
-                                "content content content content content"
-                                ". . . assign assign"`,
-          }}>
+      <TableHead>
+        <TableRow>
+            <TableCell></TableCell>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? 'right' : 'left'}
+              padding={headCell.disablePadding ? 'none' : 'normal'}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+  
+  EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+  };
+  
 
-            <Box sx={{ gridArea: 'title', backgroundColor: 'blue'}}>
-                <Typography variant='h1' align='center' sx={{ color:'white', fontSize: 'x-large', p:2}}>Uniforms</Typography>
-            </Box>
+  const customTheme = createTheme({
+      palette: {
+          primary: {
+              main: '#0000E0',
+          },
+          secondary: {
+            main: '#FFFFFF'
+        },
+        action: {
+            activatedOpacity: 0.75
+        }
+      },
+      
+  })
 
-            <Box sx={{ gridArea: 'filter'}}>Filter</Box>
 
-            <Card sx={{ gridArea: 'content'}}>
-                <CardContent sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)'}}>
-                    <Typography variant='h2'align='center' sx={{ fontWeight: 'bold', fontSize: 'large'}}>Select</Typography>
-                    <Typography variant='h2'align='center' sx={{ fontWeight: 'bold', fontSize: 'large'}}>Type</Typography>
-                    <Typography variant='h2'align='center' sx={{ fontWeight: 'bold', fontSize: 'large'}}>Uniform ID</Typography>
-                    <Typography variant='h2'align='center' sx={{ fontWeight: 'bold', fontSize: 'large'}}>Size</Typography>
-                </CardContent>
-
-                {uniforms.map(uniform => {
-                    return <UniformItem uniform={ uniform }></UniformItem>
-                })}
-            </Card>
-
+  const EnhancedTableToolbar = (props) => {
+    const { numSelected } = props;
+  
+    return (
+      <Toolbar
+        sx={{
+          pl: { sm: 2 },
+          pr: { xs: 1, sm: 1 },
+          ...(numSelected > 0 && {
+            bgcolor: (theme) =>
+              alpha(customTheme.palette.primary.main, customTheme.palette.action.activatedOpacity),
+          }),
+          ...(numSelected === 0 && {
+            bgcolor: customTheme.palette.primary.main
+          }),
+        }}
+      >
+        {numSelected > 0 ? (
+          <Typography
+            sx={{ flex: '1 1 100%', color: customTheme.palette.secondary.light }}
             
-            <Button sx={{ gridArea: 'assign'}}>
-                Assign
-            </Button>
-           
-        </Box>
-        
-    )
+            variant="subtitle1"
+            component="div"
+          >
+            {numSelected} selected
+          </Typography>
+        ) : (
+          <Typography
+            sx={{ flex: '1 1 100%', color: customTheme.palette.secondary.light }}
+            variant="h6"
+            id="tableTitle"
+            component="div"
+          >
+            Uniforms
+          </Typography>
+        )}
+  
+        {numSelected > 0 ? (
+          <Tooltip title="Delete">
+            <IconButton>
+              <DeleteIcon sx={{ color: customTheme.palette.secondary.light }} />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title="Filter list">
+            <IconButton>
+              <FilterListIcon sx={{ color: customTheme.palette.secondary.light }}/>
+            </IconButton>
+          </Tooltip>
+        )}
+      </Toolbar>
+    );
+  };
+  
+  EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+  };
+
+export default function UniformList({ uniforms }) {
+
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('uniformID'); //uniformID
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size="medium"
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+                 rows.slice().sort(getComparator(order, orderBy)) */}
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.id);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.id)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.id}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            'aria-labelledby': labelId,
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row.type}
+                      </TableCell>
+                      <TableCell align="right">{row.id}</TableCell>
+                      <TableCell align="right">{row.size}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+  
+
 }
