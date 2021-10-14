6362b846fb4cfc8e03ba220534f310fb57204809
cpaluch <cpaluch@vt.edu>
2021-10-14 12:07:02 -0400

    merge master

---
+++ mern/react-app/src/Footer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import { Grid, Link, Typography } from '@mui/material'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <Box bgcolor="text.secondary" color="white">
+                    <Container maxWidth="lg">
+                        <Grid container >
+                            <Grid>
+                                <Box margin="20px">
+                                    <Typography borderBottom={1}>Collaboration</Typography>
+                                    <Link color="inherit" underline="none">Blacksburg High School</Link>
+                                </Box>
+                            </Grid>
+                            <Grid>
+                               <Box margin="20px">
+                                    <Typography borderBottom={1}>Support</Typography>
+                                    <Link color="inherit" underline="none">Virginia Tech CS Department</Link>
+                                </Box>
+                            </Grid>
+                            <Grid>
+                                <Box margin="30px">
+                                    <Typography>Developed By: The Band Goons</Typography>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </footer>
+        )
+    }
+}
---
+++ mern/react-app/src/components/ReportsTable/ReportsTable.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import styles from './ReportsTable.module.css';
+import Typography from '@mui/material/Typography';
+
+export default function ReportsTable() {
+    const columns = [
+        { field: 'type', headerName: 'Type of Uniform', width: 200},
+        { field: 'id', headerName: 'Uniform ID', width: 200 },
+        { field: 'size', headerName: 'Size', width: 500 },
+        { field: 'lastName', headerName: 'Last name', width: 150 },
+        { field: 'firstName', headerName: 'First name', width: 150 },
+      ];
+      
+      const rows = [
+        { type: 'Marching Band', id: '1', size: 'S', lastName: 'Nachabe', firstName: 'Foad'},
+        { type: 'Drum Line', id: '2', size: 'M', lastName: 'Nelli', firstName: 'Ashish'},
+        { type: 'Clarinet', id: '3', size: 'L', lastName: 'Anderson', firstName: 'Jared'},
+        { type: 'I dont know', id: '4', size: 'XL', lastName: 'Paluch', firstName: 'Cole'},
+        { type: 'Squidwards Band', id: '5', size: 'XXL', lastName: 'Hefner', firstName: 'Noah'},
+        { type: 'Marching Band', id: '6', size: 'S', lastName: 'Nachabe', firstName: 'Foad'},
+        { type: 'Drum Line', id: '7', size: 'M', lastName: 'Nelli', firstName: 'Ashish'},
+        { type: 'Clarinet', id: '8', size: 'L', lastName: 'Anderson', firstName: 'Jared'},
+        { type: 'I dont know', id: '9', size: 'XL', lastName: 'Paluch', firstName: 'Cole'},
+        { type: 'Squidwards Band', id: '10', size: 'XXL', lastName: 'Hefner', firstName: 'Noah'},
+      ];
+      
+  return (
+    <div className={styles.wrapperComponent}>
+      <div className={styles.wrapperHeader}>
+        <Typography ml={2} mt={1} mb={1} variant="h6" color="white" fontWeight="bold" gutterBottom component="div">
+            Reports Page
+        </Typography>
+      </div>
+      <div className={styles.boxLimits}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={8}
+          rowsPerPageOptions={[8]}
+          checkboxSelection
+        />
+      </div>
+    </div>
+  );
+}
