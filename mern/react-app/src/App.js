f7c97013666204303c34fd93754a315759994ff6
cpaluch <cpaluch@vt.edu>
2021-10-14 11:56:24 -0400

    Refactor to use data grid

--- mern/react-app/src/App.js
+++ mern/react-app/src/App.js
@@ -1,22 +1,7 @@
-import UniformList from './components/UniformList';
 import React from 'react';
 
-const uniforms = [{
-  type: 'Jacket',
-  id: '61',
-  size: 'large'
-},{
-  type: 'Gloves',
-  id: '62',
-  size: 'small'
-}];
 function App() {
-  return (
-    <>
-      <UniformList uniforms={ uniforms }></UniformList>
-
-    </>
-  );
+  return null
 }
 
 export default App;
