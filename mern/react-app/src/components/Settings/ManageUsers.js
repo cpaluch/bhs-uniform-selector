4071688c006601b584a257209480f2ddcfbc84f4
anelli55 <anelli55@vt.edu>
2021-10-27 11:00:43 -0400

    setup settings page and components

---
+++ mern/react-app/src/components/Settings/ManageUsers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ManageUsers extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ManageUsers
