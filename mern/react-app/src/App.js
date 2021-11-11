6e0413933c00434b57a2b8aba5416a5db2e6c91e
Noah Hefner <noahhefner@users.noreply.github.com>
2021-11-11 13:34:24 -0500

    redirect on login and logout, handle storing and deleting access tokens

--- mern/react-app/src/App.js
+++ mern/react-app/src/App.js
@@ -9,19 +9,33 @@ import AddUniformsPage from "./AddUniformsPage";
 import ManageUsersPage from "./ManageUsersPage";
 import * as Realm from "realm-web";
 import { strict as assert } from 'assert';
+import { useHistory } from "react-router-dom";
+
 const REALM_APP_ID = "bhs-uniform-assign-utshb"; // e.g. myapp-abcde
 const app = new Realm.App({ id: REALM_APP_ID });
 
-
-function App() {
+export default function App () {
 
   const [authenticated, setAuthenticated] = useState(false);
+  const history = useHistory();
+
+  console.log(history)
+
+  useEffect(() => {
+    if (authenticated) {
+      history.push("/assign-uniforms");
+    } else {
+      history.push("/");
+    }
+  }, [authenticated]);
 
   async function handleAuthenticationAttempt (event) {
+
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const credentials = Realm.Credentials.emailPassword(data.get('email'),
                                                         data.get('password'));
+
     try {
       // Authenticate the user
       const user = await app.logIn(credentials);
@@ -31,39 +45,51 @@ function App() {
       setAuthenticated(true)
 
     } catch(err) {
+      console.log(err)
       alert("Invalid email or password!")
     }
   }
 
+  async function handleLogoutAttempt (event) {
+    try {
+      // Authenticate the user
+      const user = await app.currentUser.logOut();
+      localStorage.removeItem("token")
+      setAuthenticated(false)
+      history.push("/");
+    } catch(err) {
+      console.log(err)
+      alert("Logout failed!")
+    }
+  }
+
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <LoginPage
-              onAuthenticationAttempt={handleAuthenticationAttempt}
-            />
-          </Route>
-          <ProtectedRoute path="/reports">
-            <ReportsPage/>
-          </ProtectedRoute>
-          <ProtectedRoute path="/assign-uniforms">
-            <UniformAssignPage/>
-          </ProtectedRoute>
-          <ProtectedRoute path='/help'>
-            <HelpPage/>
-          </ProtectedRoute>
-          <ProtectedRoute path="/add-uniforms">
-            <AddUniformsPage/>
-          </ProtectedRoute>
-          <ProtectedRoute path="/manage-users">
-            <ManageUsersPage/>
-          </ProtectedRoute>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <LoginPage
+            onAuthenticationAttempt={handleAuthenticationAttempt}
+          />
+        </Route>
+        <ProtectedRoute path="/reports">
+          <ReportsPage/>
+        </ProtectedRoute>
+        <ProtectedRoute path="/assign-uniforms">
+          <UniformAssignPage/>
+        </ProtectedRoute>
+        <ProtectedRoute path='/help'>
+          <HelpPage
+            onLogoutAttempt={handleLogoutAttempt}
+          />
+        </ProtectedRoute>
+        <ProtectedRoute path="/add-uniforms">
+          <AddUniformsPage/>
+        </ProtectedRoute>
+        <ProtectedRoute path="/manage-users">
+          <ManageUsersPage/>
+        </ProtectedRoute>
+      </Switch>
+    </div>
   );
 
 }
-
-export default App;
