ba2568f4a5b11910c5783e23e976dc37e8cc2254
Noah Hefner <noah.hefner127@gmail.com>
2021-11-13 08:17:04 -0500

    adding json web tokens (in progress)

--- mern/server/routes/user.js
+++ mern/server/routes/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const userRoutes = express.Router();
 
@@ -7,33 +8,85 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-// Add a single user
-userRoutes.route("/user/add").post(function (req, res) {
+// Register a single user
+userRoutes.route("/user/register").post(function (req, res) {
+
+  // TODO: Validate request here
+
+  // Extract email and password
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Check if user already exists
   let db_connect = dbo.getDb();
-  let obj = {
-    email: req.body.email,
-    password: req.body.password
-  };
+
   db_connect
     .collection("users")
-    .insertOne(obj, function (err, response) {
-      if (err) throw err;
-      res.json(response);
+    .findOne({ email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: "Email already exists" });
+      } else {
+        // TODO: Hash password w/ bcrypt before storing it in db
+        const newUser = {
+          email : email,
+          password: password
+        };
+        db_connect
+          .collection("users")
+          .insertOne(newUser, function (err, response) {
+            if (err) throw err;
+            res.json(response);
+          })
+      }
     })
 })
 
-// Authenticate a user
-userRoutes.route("/user/authenticate").post(function (req, res) {
+// Login a user
+userRoutes.route("/user/login").post(function (req, res) {
+
+  // TODO: Validate request here
+
+  // Extract email and password
+  const email = req.body.email;
+  const password = req.body.password;
+
   let db_connect = dbo.getDb();
-  let query = {
-    email: req.body.email,
-    password: req.body.password
-  };
+
   db_connect
     .collection("users")
-    .findOne(query, function (err, response) {
-      if (err) throw err;
-      res.json(response);
+    .findOne({ email })
+    .then(user => {
+      // Check if user exists
+      if (!user) {
+        return res.status(404).json({ emailnotfound: "Email not found" });
+      }
+      // Check password
+      // TODO: Hash this password with bcrypt
+      if (password == user.password) {
+        const payload = {
+          id: user._id
+        }
+        // Sign token
+        jwt.sign(
+          payload,
+          // TODO: Move this to file with mongo URI
+          "secret here",
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
     })
 })
 
