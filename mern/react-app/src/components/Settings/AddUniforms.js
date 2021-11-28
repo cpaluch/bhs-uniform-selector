041f588f69e4ffeeaa29cdc91ac692b5ae82c641
anelli55 <anelli55@vt.edu>
2021-11-28 14:06:51 -0500

    added add uniform reqeust to backend

--- mern/react-app/src/components/Settings/AddUniforms.js
+++ mern/react-app/src/components/Settings/AddUniforms.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import axios from "axios";
 import {
   Select,
   MenuItem,
@@ -13,12 +14,20 @@ import {
 } from "@mui/material";
 
 export default function AddUniforms() {
+  //uniform type select
   const [selectVal, setSelectedVal] = useState("");
+  //uniform peice select
   const [selectValPiece, setSelectedValPiece] = useState("");
   const [headVisibility, disableHeadBox] = useState(false);
   const [waistVisibility, disableWaistBox] = useState(false);
   const [lengthVisibility, disableLengthBox] = useState(false);
   const [chestVisibility, disableChestBox] = useState(false);
+
+  const [uniformLength, setUniformLength] = useState("");
+  const [uniformChest, setUniformChest] = useState("");
+  const [uniformWaist, setUniformWaist] = useState("");
+  const [uniformHead, setUniformHead] = useState("");
+  const [uniformID, setUniformID] = useState("");
 
   let select;
   let hat = "hat";
@@ -83,7 +92,43 @@ export default function AddUniforms() {
     console.log(selectValPiece);
   }
 
-  if (selectVal == "10") {
+  function handleLengthChange(e) {
+    setUniformLength(e.target.value);
+  }
+  function handleChestChange(e) {
+    setUniformChest(e.target.value);
+  }
+  function handleHeadChange(e) {
+    setUniformHead(e.target.value);
+  }
+  function handleWaistChange(e) {
+    setUniformWaist(e.target.value);
+  }
+  function handleIDChange(e) {
+    setUniformID(e.target.value);
+  }
+
+  const addUniform = async () => {
+    const data = {
+      uniform_id: uniformID,
+      student_id: null,
+      piece: selectValPiece,
+      type: selectVal,
+      chest: uniformChest,
+      waist: uniformWaist,
+      head: uniformHead,
+      length: uniformLength,
+    };
+    const config = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    axios.post("http://localhost:5000/uniform/add", data, config);
+  };
+
+  if (selectVal == "Marching Band") {
     select = (
       <FormControl fullWidth>
         <InputLabel>Uniform Piece</InputLabel>
@@ -95,7 +140,7 @@ export default function AddUniforms() {
         </Select>
       </FormControl>
     );
-  } else if (selectVal == "20" || selectVal == "30") {
+  } else if (selectVal == "MS Concert Band" || selectVal == "HS Concert Band") {
     select = (
       <FormControl fullWidth>
         <InputLabel>Uniform Piece</InputLabel>
@@ -140,9 +185,9 @@ export default function AddUniforms() {
               <FormControl fullWidth>
                 <InputLabel>Uniform Type</InputLabel>
                 <Select label="add uniform" onChange={handleChange}>
-                  <MenuItem value={10}>Marching Band</MenuItem>
-                  <MenuItem value={20}>HS Concert Band</MenuItem>
-                  <MenuItem value={30}>MS Concert Band</MenuItem>
+                  <MenuItem value="Marching Band">Marching Band</MenuItem>
+                  <MenuItem value="HS Concert Band">HS Concert Band</MenuItem>
+                  <MenuItem value="MS Concert Band">MS Concert Band</MenuItem>
                 </Select>
               </FormControl>
             </Grid>
@@ -151,6 +196,7 @@ export default function AddUniforms() {
             </Grid>
             <Grid item xs={12} align="center" justify="center">
               <TextField
+                onChange={handleLengthChange}
                 fullWidth
                 id="outlined-basic"
                 label="Length"
@@ -160,6 +206,7 @@ export default function AddUniforms() {
             </Grid>
             <Grid item xs={12} align="center" justify="center">
               <TextField
+                onChange={handleChestChange}
                 fullWidth
                 id="outlined-basic"
                 label="Chest"
@@ -169,6 +216,7 @@ export default function AddUniforms() {
             </Grid>
             <Grid item xs={12} align="center" justify="center">
               <TextField
+                onChange={handleHeadChange}
                 fullWidth
                 id="outlined-basic"
                 label="Head"
@@ -178,6 +226,7 @@ export default function AddUniforms() {
             </Grid>
             <Grid item xs={12} align="center" justify="center">
               <TextField
+                onChange={handleWaistChange}
                 fullWidth
                 id="outlined-basic"
                 label="Waist"
@@ -187,6 +236,7 @@ export default function AddUniforms() {
             </Grid>
             <Grid item xs={12} align="center" justify="center">
               <TextField
+                onChange={handleIDChange}
                 fullWidth
                 id="outlined-basic"
                 label="Uniform ID"
@@ -194,7 +244,14 @@ export default function AddUniforms() {
               />
             </Grid>
             <Grid item xs={12} align="right" justify="right">
-              <Button variant="contained">Add</Button>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  addUniform();
+                }}
+              >
+                Add
+              </Button>
             </Grid>
           </Grid>
         </Box>
